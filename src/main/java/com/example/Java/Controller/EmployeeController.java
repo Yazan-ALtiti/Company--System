95bee22566e813fc241e127b6abaa9d01641763c
Yazan70 <titiyazan14@gmail.com>
2023-01-23 16:54:18 +0300

    add teacher

---
+++ src/main/java/com/example/Java/Controller/EmployeeController.java
@@ -0,0 +1,41 @@
+package com.example.Java.Controller;
+
+import com.example.Java.models.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@Controller
+
+public class EmployeeController {
+    ArrayList<Employee>EmloyeeData=new ArrayList<Employee>();
+    @ResponseBody
+    @PostMapping("/employee/add")
+    public String AddEmployee(@RequestParam String name,@RequestParam double salary ){
+        Employee employee=new Employee(name,EmloyeeData.size()+1,salary);
+        EmloyeeData.add(employee);
+        return "successful";
+    }
+    @ResponseBody
+    @GetMapping("/employee/all")
+    public ArrayList<Employee>AllEmployee(){
+        return this.EmloyeeData;
+
+    }
+    @ResponseBody
+    @PutMapping("/employee/update/{id}")
+    public String UbdateEmployee(@RequestParam String name,@RequestParam double salary,@PathVariable int id){
+        Employee employee=EmloyeeData.get(id);
+        employee.setName(name);
+        employee.setSalary(salary);
+        // other way
+     //   Employee newEmployee=new Employee(name,id,salary);
+     //   EmloyeeData.set(id,newEmployee);
+
+
+     return "update Successfully";
+    }
+
+
+}
